7fedbde70b181de9b11cf9e84a76622bb0c452ac
pd241008 <142252602+pd241008@users.noreply.github.com>
2025-07-28 23:47:58 +0530

    feat: initialize Next.js project with TypeScript, Tailwind CSS, and Firebase
    - Add package.json with dependencies and scripts for Next.js, React, and Firebase
    - Create postcss.config.mjs for Tailwind CSS integration
    - Add global CSS file with Tailwind CSS import
    - Implement RootLayout component for application layout
    - Create Home page with InputBox component
    - Develop InputBox component for user input with form handling
    - Add TypeScript configuration for the project

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import InputBox from "@/components/InputBox";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <div className="text-center text-2xl font-extrabold  mt-10 p-2">Home</div>
+      <main>
+        <InputBox />
+      </main>
+    </>
+  );
+};
+
+export default Home;
